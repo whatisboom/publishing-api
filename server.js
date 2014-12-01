9166990ce480be212fc2e7c762bf63bf64c58780
Brandon Jernigan <brandon@piqora.com>
2014-12-01 12:09:09 -0600

    added server base

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8889;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({
+        meta: {
+            message: "Working!"
+        }
+    })
+});
+
+app.use('/api', router);
+
+app.listen(port);
