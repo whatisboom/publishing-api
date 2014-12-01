64020ff1ab59a01a37ff144ae8935b7bf4f2355c
Brandon Jernigan <brandon@piqora.com>
2014-12-01 12:34:12 -0600

    mongo addition, added config file to ignore

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8889;
