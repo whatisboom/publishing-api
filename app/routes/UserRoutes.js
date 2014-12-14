3cda057403699dcc74c8c40851247ec9309115b1
Brandon Jernigan <brandon@piqora.com>
2014-12-14 11:52:31 -0800

    user routes

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -1,19 +1,21 @@
 var User = require('../models/user.js');
-
+var extend = require('extend');
 module.exports = function(router) {
     router.route('/users')
     .get(function(req, res) {
-        res.json({
-            meta: {
-
-            },
-            data: {
-                users: [{
-                    name: 'Brandon Jernigan',
-                    email: 'brandon@whatisboom.com'
-                }]
+        User.find(function(err, users) {
+            if (err) {
+                res.status(500);
             }
-        })
+            res.json({
+                meta: {
+                    error: err
+                },
+                data: {
+                    users: users
+                }
+            })
+        });
     })
     .post(function(req, res) {
 
@@ -39,6 +41,52 @@ module.exports = function(router) {
                 }
             })
         });
+    });
+
+    router.route('/users/:user_id')
+    .get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) {
+                res.status(500)
+            }
+            res.json({
+                meta: {
+                    error: err
+                },
+                data: {
+                    users: [user]
+                }
+            })
+        });
+    })
+    .put(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err) {
+                res.status(500)
+            }
+
+            extend(user, req.body);
+
+            user.save(function(err) {
+                if (err) {
+                    res.status(500);
+                }
+
+                res.json({
+                    data: {
+                        users: [user]
+                    }
+                })
+            });
+        });
+    })
+    .delete(function(req, res) {
+        res.status(403);
+        res.json({
+            meta: {
+                error: 'No.'
+            }
+        })
     });
 
     return router;
