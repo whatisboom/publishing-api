2058adfe2664b71dc32dfc884f062c8c497aa24b
Brandon Jernigan <brandon@piqora.com>
2014-12-15 09:59:47 -0800

    removed config files

--- app/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtTokenSecret: 'what is piqora',
-    sessionSecret: 'this is the session secret 123456'
-};
--- app/config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    database: 'mongodb://api:classyrabbit420@ds051990.mongolab.com:51990/publishing-api'
-};
