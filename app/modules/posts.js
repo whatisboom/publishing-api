84f62083012162cf4a7cd3dadab2cf40e4b27f96
Brandon Jernigan <brandon@piqora.com>
2014-12-21 04:12:13 -0800

    updated args for posts create

--- app/modules/posts.js
+++ app/modules/posts.js
@@ -1,4 +1,5 @@
 var Post = require('../models/post.js');
+var Media = require('../models/media.js');
 var Utils = require('../modules/Utils.js');
 
 module.exports = {
@@ -6,12 +7,42 @@ module.exports = {
         return function(req, res) {
             var results = [];
             var payload = req.body.data.posts;
-
+            var mediaIds = [];
             for (var i = payload.length - 1; i >= 0; i--) {
                 var post = payload[i];
+                mediaIds.push(post.mediaId);
+                results.push({
+                    user: req.user.id,
+                    network: post.network,
+                    mediaId: post.mediaId,
+                    description: post.description,
+                    url: post.url
+                })
             };
 
-            res.status(501).json(Utils.error(501, "Not yet implemented."));
+            Media.find({
+                _id: {
+                    $in: mediaIds
+                }
+            }, function(err, media) {
+                if (media.length === payload.length) {
+                    Post.create(payload, function(err, posts) {
+                        if (err) {
+                            res.status(500).json(Utils.error(500, err));
+                        }
+                        else {
+                            res.status(200).json({
+                                data: {
+                                    posts: posts
+                                }
+                            });
+                        }
+                    })
+                }
+                else {
+                    res.status(404).json(Utils.error(404, "One or more invalid mediaId."))
+                }
+            });
         }
     },
     read: function() {
